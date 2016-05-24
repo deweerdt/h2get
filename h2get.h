7030a5ce1f8fea72061128bec923362f4caf1b4c
Frederik Deweerdt <frederik.deweerdt@gmail.com>
2016-05-23 17:22:17 -0700

    Initial commit

---
+++ h2get.h
@@ -0,0 +1,266 @@
+#ifndef H2GET_H_
+#define H2GET_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdarg.h>
+#include <sys/socket.h>
+
+#define H2GET_HEADERS_SETTINGS_FLAGS_ACK 0x1
+#define H2GET_HEADERS_SETTINGS_HEADER_TABLE_SIZE 0x1
+#define H2GET_HEADERS_SETTINGS_ENABLE_PUSH 0x2
+#define H2GET_HEADERS_SETTINGS_MAX_CONCURRENT_STREAMS 0x3
+#define H2GET_HEADERS_SETTINGS_INITIAL_WINDOW_SIZE 0x4
+#define H2GET_HEADERS_SETTINGS_MAX_FRAME_SIZE 0x5
+#define H2GET_HEADERS_SETTINGS_MAX_HEADER_LIST_SIZE 0x6
+
+#define H2GET_HEADERS_DATA 0x0
+#define H2GET_HEADERS_HEADERS 0x1
+#define H2GET_HEADERS_PRIORITY 0x2
+#define H2GET_HEADERS_RST_STREAM 0x3
+#define H2GET_HEADERS_SETTINGS 0x4
+#define H2GET_HEADERS_PUSH_PROMISE 0x5
+#define H2GET_HEADERS_PING 0x6
+#define H2GET_HEADERS_GOAWAY 0x7
+#define H2GET_HEADERS_WINDOW_UPDATE 0x8
+#define H2GET_HEADERS_CONTINUATION 0x9
+#define H2GET_HEADERS_MAX 0xa
+
+#define H2GET_ERR_NO_ERROR 0x0
+#define H2GET_ERR_PROTOCOL_ERROR 0x1
+#define H2GET_ERR_INTERNAL_ERROR 0x2
+#define H2GET_ERR_FLOW_CONTROL_ERROR 0x3
+#define H2GET_ERR_SETTINGS_TIMEOUT 0x4
+#define H2GET_ERR_STREAM_CLOSED 0x5
+#define H2GET_ERR_FRAME_SIZE_ERROR 0x6
+#define H2GET_ERR_REFUSED_STREAM 0x7
+#define H2GET_ERR_CANCEL 0x8
+#define H2GET_ERR_COMPRESSION_ERROR 0x9
+#define H2GET_ERR_CONNECT_ERROR 0xa
+#define H2GET_ERR_ENHANCE_YOUR_CALM 0xb
+#define H2GET_ERR_INADEQUATE_SECURITY 0xc
+#define H2GET_ERR_HTTP_1_1_REQUIRED 0xd
+
+#define H2GET_HEADERS_HEADERS_FLAG_END_STREAM 0x1
+#define H2GET_HEADERS_HEADERS_FLAG_END_HEADERS 0x4
+#define H2GET_HEADERS_HEADERS_FLAG_PADDED 0x8
+#define H2GET_HEADERS_HEADERS_FLAG_PRIORITY 0x20
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+#include <ctype.h>
+#include <stdio.h>
+static inline void dump_line(char *data, int offset, int limit)
+{
+    int i;
+
+    printf("%03x:", offset);
+    for (i = 0; i < limit; i++) {
+        printf(" %02x", (unsigned char)data[offset + i]);
+    }
+    for (i = 0; i + limit < 16; i++) {
+        printf("   ");
+    }
+    printf(" ");
+    for (i = 0; i < limit; i++) {
+        printf("%c", isprint(data[offset + i]) ? data[offset+i]:'.');
+    }
+    printf("\n");
+}
+
+static inline void dump_zone(void *buf, int len)
+{
+    int i;
+    char *data = buf;
+
+    printf("================================================================================\n");
+    for (i = 0; i < len; i += 16) {
+        int limit;
+        limit = 16;
+        if (i + limit > len)
+            limit = len - i;
+        dump_line(data, i, limit);
+    }
+    printf("================================================================================\n");
+}
+
+#define H2GET_DOT_STAR_PRINTF(s) ((int)(s)->len), ((s)->buf)
+#define H2GET_STRLIT(s) (s), (sizeof((s)) - 1)
+#define H2GET_BUFLIT(s) ((struct h2get_buf){(s), (sizeof((s)) - 1)})
+#define H2GET_BUFSTR(s) ((struct h2get_buf){(s), strlen(s)})
+#define H2GET_BUF(b, l) ((struct h2get_buf){(void *)b, l})
+#define H2GET_BUF_NULL ((struct h2get_buf){})
+#define H2GET_TO_STR_ALLOCA(b) ({ \
+        char *ret_ = alloca((b).len + 1); \
+        memcpy(ret_, (b).buf, (b).len); \
+        ret_[(b).len] = '\0'; \
+        ret_;})
+
+struct h2get_buf {
+    char *buf;
+    size_t len;
+};
+
+static inline int h2get_buf_printf(struct h2get_buf *out, char *fmt, ...)
+{
+    int ret;
+    size_t newsz;
+    char *p;
+    va_list a1, a2;
+    va_start(a1, fmt);
+    va_copy(a2, a1);
+
+    ret = vsnprintf(NULL, 0, fmt, a1);
+    if (ret < 0) {
+        return ret;
+    }
+    newsz= out->len + ret + 1;
+    p = realloc(out->buf, newsz);
+    if (!p) {
+        return -1;
+    }
+    out->buf = p;
+    ret = vsprintf(out->buf + out->len, fmt, a2);
+    out->len = newsz;
+    return ret;
+}
+
+enum h2get_conn_state {
+    H2GET_CONN_STATE_INIT,
+    H2GET_CONN_STATE_CONNECT,
+};
+
+struct h2get_conn {
+    int fd;
+    int protocol;
+    int socktype;
+    struct {
+        struct sockaddr_storage sa_storage;
+        socklen_t len;
+        struct sockaddr *sa;
+    } sa;
+    enum h2get_conn_state state;
+    struct h2get_buf servername;
+    void *priv;
+};
+
+enum h2get_transport {
+    H2GET_TRANSPORT_UNIX,
+    H2GET_TRANSPORT_PLAIN,
+    H2GET_TRANSPORT_SSL,
+};
+
+struct h2get_ops {
+    enum h2get_transport xprt;
+    void *(*init)(void);
+    int (*connect)(struct h2get_conn *, void *);
+    int (*write)(struct h2get_conn *, struct h2get_buf *bufs, size_t nr_bufs);
+    int (*read)(struct h2get_conn *, struct h2get_buf *buf, int tout);
+    int (*close)(struct h2get_conn *, void *);
+    void (*fini)(void *);
+};
+
+static inline void *memdup(void *src, size_t len)
+{
+    void *dst = malloc(len);
+    memcpy(dst, src, len);
+    return dst;
+}
+
+extern struct h2get_ops plain_ops;
+extern struct h2get_ops ssl_ops;
+
+struct list {
+    struct list *next, *prev;
+};
+
+static inline int list_empty(struct list *l)
+{
+	return l->next == l;
+}
+static inline void list_init(struct list *n)
+{
+	n->next = n;
+	n->prev = n;
+}
+
+static inline void list_del(struct list *n)
+{
+	n->next->prev = n->prev;
+	n->prev->next = n->next;
+}
+
+static inline struct list *list_tail(struct list *l)
+{
+    return l->prev;
+}
+static inline void list_add(struct list *l, struct list *n)
+{
+	n->next = l->next;
+	n->next->prev = n;
+	l->next = n;
+	n->prev = l;
+}
+
+static inline void list_add_tail(struct list *l, struct list *n)
+{
+	list_add(l->prev, n);
+}
+
+#undef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#else
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+#define container_of(ptr, type, member) ({			\
+		const __typeof__( ((type *)0)->member ) *__mptr = (ptr);	\
+		(type *)( (char *)__mptr - offsetof(type,member) );})
+
+uint8_t *read_bits(uint8_t *buf, uint8_t nr_bits, uint32_t *value, uint8_t *offset);
+
+struct h2get_h2_header {
+    unsigned int len : 24;
+    unsigned int type : 8;
+    unsigned int flags : 8;
+    unsigned int r : 1;
+    unsigned int stream_id : 31;
+    char payload[0];
+} __attribute__((packed));
+
+struct h2get_h2_setting {
+    uint16_t id;
+    uint32_t value;
+} __attribute__((packed));
+
+struct h2get_h2_window_update {
+    unsigned int reserved : 1;
+    unsigned int increment : 31;
+} __attribute__((packed));
+
+struct h2get_h2_goaway {
+    unsigned int reserved : 1;
+    unsigned int last_stream_id : 31;
+    uint32_t error_code;
+    char additional_debug_data[];
+} __attribute__((packed));
+
+
+struct h2get_ctx;
+void h2get_ctx_init(struct h2get_ctx *ctx);
+void h2get_ctx_on_settings_ack(struct h2get_ctx *ctx);
+struct h2get_h2_settings;
+int h2get_ctx_on_peer_settings(struct h2get_ctx *ctx, struct h2get_h2_header *h, char *payload, int plen);
+int h2get_connect(struct h2get_ctx *ctx, struct h2get_buf url_buf, const char **err);
+int h2get_close(struct h2get_ctx *ctx);
+int h2get_send_settings(struct h2get_ctx *ctx, const char **err);
+int h2get_send_prefix(struct h2get_ctx *ctx, const char **err);
+int h2get_send_windows_update(struct h2get_ctx *ctx, uint32_t stream_id, uint32_t increment, const char **err);
+int h2get_get(struct h2get_ctx *ctx, const char *path, const char **err);
+const char *h2get_render_error_code(uint32_t err);
+
+#endif /* H2GET_H_ */
+/* vim: set expandtab ts=4 sw=4: */
---
+++ h2get_mruby.c
@@ -0,0 +1,411 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "h2get.h"
+#include "h2get_priv.h"
+
+#include "mruby.h"
+#include "mruby/array.h"
+#include "mruby/class.h"
+#include "mruby/compile.h"
+#include "mruby/data.h"
+#include "mruby/error.h"
+#include "mruby/string.h"
+
+struct h2get_mruby {
+    struct h2get_ctx ctx;
+};
+
+struct h2get_mruby_frame {
+    struct h2get_ctx *ctx;
+    struct h2get_buf payload;
+    struct h2get_h2_header header;
+};
+
+static char const H2GET_MRUBY_KEY[] = "$h2get_mruby_type";
+static char const H2GET_MRUBY_FRAME_KEY[] = "$h2get_mruby_frame_type";
+
+static const struct mrb_data_type h2get_mruby_type = {
+    H2GET_MRUBY_KEY, mrb_free,
+};
+
+static const struct mrb_data_type h2get_mruby_frame_type = {
+    H2GET_MRUBY_FRAME_KEY, mrb_free,
+};
+
+static struct RClass *h2get_mruby_frame;
+
+#define H2GET_MRUBY_ASSERT_ARGS(expected_argc_)                                                                        \
+    do {                                                                                                               \
+        mrb_value *argv;                                                                                               \
+        mrb_int argc;                                                                                                  \
+        int iargc;                                                                                                     \
+        mrb_get_args(mrb, "*", &argv, &argc);                                                                          \
+        iargc = (int)argc;                                                                                             \
+        if (iargc != expected_argc_) {                                                                                 \
+            mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong number of arguments");                                             \
+        }                                                                                                              \
+    } while (0)
+
+static mrb_value h2get_mruby_init(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+    if (h2g) {
+        mrb_free(mrb, h2g);
+    }
+    H2GET_MRUBY_ASSERT_ARGS(0);
+
+    mrb_data_init(self, NULL, &h2get_mruby_type);
+
+    h2g = (struct h2get_mruby *)mrb_malloc(mrb, sizeof(*h2g));
+
+    h2get_ctx_init(&h2g->ctx);
+
+    mrb_data_init(self, h2g, &h2get_mruby_type);
+
+    return self;
+}
+
+static mrb_value h2get_mruby_connect(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    const char *err = NULL;
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    H2GET_MRUBY_ASSERT_ARGS(1);
+
+    char *url = NULL;
+    mrb_get_args(mrb, "z", &url);
+
+    h2get_connect(&h2g->ctx, H2GET_BUFSTR(url), &err);
+    if (err) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_close(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    h2get_close(&h2g->ctx);
+
+    return mrb_nil_value();
+}
+
+static mrb_value create_frame(mrb_state *mrb, struct h2get_ctx *ctx, struct h2get_h2_header *header,
+                              struct h2get_buf *payload)
+{
+    struct h2get_mruby_frame *h2g_frame;
+    mrb_value frame;
+
+    frame = mrb_obj_new(mrb, h2get_mruby_frame, 0, NULL);
+
+    h2g_frame = (struct h2get_mruby_frame *)mrb_malloc(mrb, sizeof(*h2g_frame));
+    h2g_frame->ctx = ctx;
+    h2g_frame->header = *header;
+    h2g_frame->payload = *payload;
+
+    mrb_data_init(frame, h2g_frame, &h2get_mruby_frame_type);
+
+    return frame;
+}
+
+static mrb_value h2get_mruby_read(mrb_state *mrb, mrb_value self)
+{
+    int ret;
+    struct h2get_mruby *h2g;
+    struct h2get_h2_header header;
+    struct h2get_buf payload;
+    int timeout;
+    const char *err;
+    mrb_value *argv;
+    mrb_int argc;
+    int iargc;
+
+    mrb_get_args(mrb, "*", &argv, &argc);
+
+    iargc = (int)argc;
+    if (iargc == 0) {
+        timeout = -1;
+    } else {
+        mrb_get_args(mrb, "i", &timeout);
+    }
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    ret = h2get_read_one_frame(&h2g->ctx, &header, &payload, timeout, &err);
+    if (ret < 0) {
+        mrb_value exc;
+
+        if (err == err_read_timeout) {
+            return mrb_nil_value();
+        }
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+
+        return mrb_nil_value();
+    }
+    return create_frame(mrb, &h2g->ctx, &header, &payload);
+}
+
+static mrb_value h2get_mruby_send_settings(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    const char *err;
+    int ret;
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    ret = h2get_send_settings(&h2g->ctx, &err);
+    if (ret < 0) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_send_prefix(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    const char *err;
+    int ret;
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    ret = h2get_send_prefix(&h2g->ctx, &err);
+    if (ret < 0) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_get(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    const char *err;
+    int ret;
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    H2GET_MRUBY_ASSERT_ARGS(1);
+
+    char *path = NULL;
+    mrb_get_args(mrb, "z", &path);
+
+    ret = h2get_get(&h2g->ctx, path, &err);
+    if (ret < 0) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_send_window_update(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    int ret;
+    const char *err;
+    mrb_int mrb_stream_id, mrb_increment;
+    uint32_t stream_id, increment;
+
+    mrb_get_args(mrb, "ii", &mrb_stream_id, &mrb_increment);
+    stream_id = (uint32_t)mrb_stream_id;
+    increment = (uint32_t)mrb_increment;
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    ret = h2get_send_windows_update(&h2g->ctx, stream_id, increment, &err);
+    if (ret < 0) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_send_settings_ack(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby *h2g;
+    int ret;
+
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+
+    ret = h2get_send_settings_ack(&h2g->ctx, 1);
+    if (ret < 0) {
+        mrb_value exc;
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, strerror(errno), strlen(strerror(errno)));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_on_settings(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby_frame *h2g_frame;
+    struct h2get_mruby *h2g;
+    mrb_value frame_mrbv;
+    int ret;
+
+    mrb_get_args(mrb, "o", &frame_mrbv);
+    h2g = (struct h2get_mruby *)DATA_PTR(self);
+    h2g_frame = (struct h2get_mruby_frame *)DATA_PTR(frame_mrbv);
+    ret = h2get_ctx_on_peer_settings(&h2g->ctx, &h2g_frame->header, h2g_frame->payload.buf, h2g_frame->payload.len);
+    if (ret < 0) {
+        const char *err;
+        mrb_value exc;
+        err = h2get_render_error_code(-ret);
+        exc = mrb_exc_new(mrb, E_RUNTIME_ERROR, err, strlen(err));
+        mrb->exc = mrb_obj_ptr(exc);
+    }
+    return mrb_nil_value();
+}
+
+static mrb_value h2get_mruby_on_settings_ack(mrb_state *mrb, mrb_value self) { return mrb_nil_value(); }
+
+static mrb_value h2get_mruby_kernel_sleep(mrb_state *mrb, mrb_value self)
+{
+    time_t beg, end;
+    mrb_value *argv;
+    mrb_int argc;
+    int iargc;
+
+    beg = time(0);
+    mrb_get_args(mrb, "*", &argv, &argc);
+
+    iargc = (int)argc;
+
+    /* not implemented forever sleep (called without an argument)*/
+    if (iargc == 0 || iargc >= 2) {
+        mrb_raise(mrb, E_ARGUMENT_ERROR, "wrong number of arguments");
+    }
+
+    if (mrb_fixnum_p(argv[0]) && mrb_fixnum(argv[0]) >= 0) {
+        sleep(mrb_fixnum(argv[0]));
+    } else {
+        mrb_raise(mrb, E_ARGUMENT_ERROR, "time interval must be positive");
+    }
+    end = time(0) - beg;
+
+    return mrb_fixnum_value(end);
+}
+
+/* Frame */
+
+static mrb_value h2get_mruby_frame_type_str(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby_frame *h2g_frame;
+    const char *ret;
+
+    h2g_frame = (struct h2get_mruby_frame *)DATA_PTR(self);
+
+    ret = h2get_frame_type_to_str(h2g_frame->header.type);
+    return mrb_str_new(mrb, ret, strlen(ret));
+}
+
+static mrb_value h2get_mruby_frame_type_num(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby_frame *h2g_frame;
+
+    h2g_frame = (struct h2get_mruby_frame *)DATA_PTR(self);
+
+    return mrb_fixnum_value(h2g_frame->header.type);
+}
+
+static mrb_value h2get_mruby_frame_to_s(mrb_state *mrb, mrb_value self)
+{
+    struct h2get_mruby_frame *h2g_frame;
+    char *buf = NULL;
+    int ret;
+    struct h2get_buf out;
+
+    h2g_frame = (struct h2get_mruby_frame *)DATA_PTR(self);
+    ret = asprintf(&buf, "%s frame <length=%zu, flags=0x%02x, stream_id=%" PRIu32 ">",
+                   h2get_frame_type_to_str(h2g_frame->header.type), h2g_frame->payload.len, h2g_frame->header.flags,
+                   ntohl(h2g_frame->header.stream_id << 1));
+    out = H2GET_BUF(buf, ret);
+    h2get_frame_get_renderer(h2g_frame->header.type)(h2g_frame->ctx, &out, &h2g_frame->header, h2g_frame->payload.buf,
+                                                     h2g_frame->payload.len);
+
+    return mrb_str_new(mrb, out.buf, out.len);
+}
+
+void run_mruby(const char *rbfile, int argc, char **argv)
+{
+    mrb_value ARGV;
+    int i;
+    mrb_state *mrb = mrb_open();
+
+    ARGV = mrb_ary_new_capa(mrb, argc);
+    for (i = 0; i < argc; i++) {
+        char *utf8 = mrb_utf8_from_locale(argv[i], -1);
+        if (utf8) {
+            mrb_ary_push(mrb, ARGV, mrb_str_new_cstr(mrb, utf8));
+            mrb_utf8_free(utf8);
+        }
+    }
+    mrb_define_global_const(mrb, "ARGV", ARGV);
+    struct RClass *h2get_mruby = mrb_define_class(mrb, "H2", mrb->object_class);
+    MRB_SET_INSTANCE_TT(h2get_mruby, MRB_TT_DATA);
+    h2get_mruby_frame = mrb_define_class(mrb, "H2Frame", mrb->object_class);
+    MRB_SET_INSTANCE_TT(h2get_mruby, MRB_TT_DATA);
+
+    mrb_define_method(mrb, h2get_mruby, "initialize", h2get_mruby_init, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby, "connect", h2get_mruby_connect, MRB_ARGS_ARG(1, 0));
+
+    mrb_define_method(mrb, h2get_mruby, "send_prefix", h2get_mruby_send_prefix, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby, "send_settings", h2get_mruby_send_settings, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby, "send_settings_ack", h2get_mruby_send_settings_ack, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby, "send_window_update", h2get_mruby_send_window_update, MRB_ARGS_ARG(2, 0));
+
+    mrb_define_method(mrb, h2get_mruby, "get", h2get_mruby_get, MRB_ARGS_ARG(1, 0));
+
+    mrb_define_method(mrb, h2get_mruby, "on_settings", h2get_mruby_on_settings, MRB_ARGS_ARG(1, 0));
+    mrb_define_method(mrb, h2get_mruby, "on_settings_ack", h2get_mruby_on_settings_ack, MRB_ARGS_ARG(1, 0));
+    mrb_define_method(mrb, h2get_mruby, "read", h2get_mruby_read, MRB_ARGS_ARG(0, 1));
+    mrb_define_method(mrb, h2get_mruby, "close", h2get_mruby_close, MRB_ARGS_ARG(1, 0));
+    mrb_define_method(mrb, mrb->kernel_module, "sleep", h2get_mruby_kernel_sleep, MRB_ARGS_ARG(1, 0));
+
+    mrb_define_method(mrb, h2get_mruby_frame, "type", h2get_mruby_frame_type_str, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby_frame, "type_num", h2get_mruby_frame_type_num, MRB_ARGS_ARG(0, 0));
+    mrb_define_method(mrb, h2get_mruby_frame, "to_s", h2get_mruby_frame_to_s, MRB_ARGS_ARG(0, 0));
+
+    FILE *f = fopen(rbfile, "r");
+    if (!f) {
+        printf("Failed to open file `%s`: %s\n", rbfile, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    mrb_value obj = mrb_load_file(mrb, f);
+    fclose(f);
+    fflush(stdout);
+    fflush(stderr);
+
+    if (mrb->exc) {
+        // Error
+        obj = mrb_funcall(mrb, mrb_obj_value(mrb->exc), "inspect", 0);
+        fwrite(RSTRING_PTR(obj), RSTRING_LEN(obj), 1, stdout);
+        putc('\n', stdout);
+        exit(EXIT_FAILURE);
+    }
+
+    mrb_close(mrb);
+    return;
+}
