cd296a04e907fe1d7e85eb8ee7a63d0e21721dc2
Kazuho Oku <kazuhooku@gmail.com>
2022-07-12 15:32:29 +0900

    follow API changes

--- src/h2get_mruby.c
+++ src/h2get_mruby.c
@@ -598,7 +598,7 @@ static mrb_value h2get_mruby_send_headers(mrb_state *mrb, mrb_value self)
     h2g = (struct h2get_mruby *)DATA_PTR(self);
 
     header_keys = mrb_hash_keys(mrb, headers);
-    mrb_int headers_len = mrb_ary_len(mrb, header_keys);
+    mrb_int headers_len = RARRAY_LEN(header_keys);
 
     struct h2get_buf h2_headers[headers_len * 2];
     for (int i = 0; i < headers_len; i++) {
@@ -653,7 +653,7 @@ static mrb_value h2get_mruby_send_continuation(mrb_state *mrb, mrb_value self)
     h2g = (struct h2get_mruby *)DATA_PTR(self);
 
     header_keys = mrb_hash_keys(mrb, headers);
-    mrb_int headers_len = mrb_ary_len(mrb, header_keys);
+    mrb_int headers_len = RARRAY_LEN(header_keys);
 
     struct h2get_buf h2_headers[headers_len * 2];
     for (int i = 0; i < headers_len; i++) {
@@ -860,7 +860,7 @@ static mrb_value h2get_mruby_frame_to_s(mrb_state *mrb, mrb_value self)
 
     h2g_frame = (struct h2get_mruby_frame *)DATA_PTR(self);
     ret = asprintf(&buf, "%s frame <length=%d, flags=0x%02x, stream_id=%" PRIu32 ">",
-                   h2get_frame_type_to_str(h2g_frame->header.type), RSTRING_LEN(h2g_frame->payload),
+                   h2get_frame_type_to_str(h2g_frame->header.type), (int)RSTRING_LEN(h2g_frame->payload),
                    h2g_frame->header.flags, ntohl(h2g_frame->header.stream_id << 1));
     out = H2GET_BUF(buf, ret);
     h2get_frame_get_renderer(h2g_frame->header.type)(h2g_frame->ctx, &out, &h2g_frame->header,
